c0d92dc5a52c65e9ba86b03fd788e381b193ea4b
Fernando De Souza <fernandodesouza@users.noreply.github.com>
2025-04-06 13:36:28 +0200

    Revert "Modernize dependencies and improve code quality - Update dependencies to latest stable versions and improve code structure"
    This reverts commit 9d54b12f0faabdf9b2b7c69f4e24d8c37b9672d9.

--- src/main/java/com/haters/games/GameController.java
+++ src/main/java/com/haters/games/GameController.java
@@ -1,53 +1,49 @@
 package com.haters.games;
 
+
 import org.jbox2d.common.IViewportTransform;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.haters.games.input.GameInputStream;
 import com.haters.games.input.NetworkInputStream;
 import com.haters.games.output.NetworkOutputStream;
+
 import com.haters.games.physics.GameLogic;
 import com.haters.games.physics.SpaceWorld;
 
-import java.time.Instant;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Date;
+
 
 public class GameController implements Runnable {
 
-    private static final Logger logger = LoggerFactory.getLogger(GameController.class);
+
     private static final int DEFAULT_FPS = 60;
-    private static final int POSITION_ITERATIONS = 3;
-    private static final int VELOCITY_ITERATIONS = 8;
+    private static final int PositionIterations = 3;
+    private static final int VelocityIterations = 8;
 
     private final IViewportTransform transform = null;
     private final Vec2 initPosition = new Vec2(0, 0);
     private final float initScale = 7;
     private float frameRate;
-    private final AtomicBoolean running = new AtomicBoolean(true);
+
     private final Thread animation;
 
     public GameController() {
-        this.animation = new Thread(this, "game-loop");
+        super();
+        this.animation = new Thread(this, "thread-1");
     }
 
     public float getFps() {
         return frameRate;
     }
 
-    public void stop() {
-        running.set(false);
-        animation.interrupt();
-    }
-
-    @Override
     public void run() {
-        long lastPrintTime = 0;
+        long lastprinttime = 0;
 
         final SpaceWorld spaceWorld = new SpaceWorld(this, new World(new Vec2(0.0f, 0.0f)));
         final GameInputStream istream = new NetworkInputStream();
+
         GameLogic logic = new GameLogic(spaceWorld, istream, new NetworkOutputStream());
         logic.init();
 
@@ -59,9 +55,8 @@ public class GameController implements Runnable {
         beforeTime = updateTime = System.nanoTime();
         sleepTime = 0;
 
-        logger.info("Game loop started");
+        while (true) {
 
-        while (running.get()) {
             timeSpent = beforeTime - updateTime;
             if (timeSpent > 0) {
                 timeInSecs = timeSpent * 1.0f / 1000000000.0f;
@@ -71,37 +66,34 @@ public class GameController implements Runnable {
                 updateTime = System.nanoTime();
             }
 
-            try {
-                istream.processEvents();
-                logic.step(1f / frameRate, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
-                logic.afterStep();
+            istream.processEvents();
+            logic.step(1f / frameRate, VelocityIterations, PositionIterations);
+            logic.afterStep();
 
-                afterTime = System.nanoTime();
-                timeDiff = afterTime - beforeTime;
-                sleepTime = (1000000000 / targetFrameRate - timeDiff) / 1000000;
-                
-                if (sleepTime > 0) {
+
+            afterTime = System.nanoTime();
+
+            timeDiff = afterTime - beforeTime;
+            sleepTime = (1000000000 / targetFrameRate - timeDiff) / 1000000;
+            if (sleepTime > 0) {
+                try {
                     Thread.sleep(sleepTime);
+                } catch (InterruptedException ex) {
                 }
-
-                beforeTime = System.nanoTime();
-
-                if (Instant.now().toEpochMilli() - lastPrintTime > 3000) {
-                    logger.debug("Current frame rate: {}", frameRate);
-                    lastPrintTime = Instant.now().toEpochMilli();
-                }
-            } catch (InterruptedException ex) {
-                logger.warn("Game loop interrupted", ex);
-                break;
-            } catch (Exception ex) {
-                logger.error("Error in game loop", ex);
             }
-        }
 
-        logger.info("Game loop stopped");
+            beforeTime = System.nanoTime();
+
+            if (new Date().getTime() - lastprinttime > 3000) {
+                System.out.println("frame rate: " + frameRate);
+                lastprinttime = new Date().getTime();
+            }
+
+        }
     }
 
     public void start() {
         animation.start();
     }
+
 }
